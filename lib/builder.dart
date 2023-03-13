54101554ffae0e97125936a87bc3fcd1f1ffd5f6
Christoph Bühler <christoph@smartive.ch>
2023-03-13 13:39:35 +0700

    feat: add screenbuilder that creates app breakpoints and a responsive value helper

--- lib/builder.dart
+++ lib/builder.dart
@@ -3,9 +3,9 @@ library builder;
 import 'package:build/build.dart';
 
 import 'src/color_builder.dart';
+import 'src/screen_builder.dart';
 import 'src/spatial_builder.dart';
 
 Builder colorBuilder(BuilderOptions options) => ColorBuilder();
 Builder spatialBuilder(BuilderOptions options) => SpatialBuilder();
-// TODO: border radius builder
-// TODO: screens / breakpoint radius builder
+Builder screenBuilder(BuilderOptions options) => ScreenBuilder();
